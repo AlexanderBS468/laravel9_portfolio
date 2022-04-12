a352d458af97283f5dd8c8d532d8bc0070ecd0d6
AlexBS <alex.sad.468@gmail.com>
2022-04-12 09:47:31 +0100

    Add tailwindcss and laravel-mix

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.export = {
+  content: [
+    './storage/framework/views/*.php',
+    './resources/**/*.blade.php',
+    './resources/**/*.js',
+    './resources/**/*.vue',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
